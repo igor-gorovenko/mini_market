fda580b8dfb50eb5b4bd650bc462746ace737d9a
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-01 16:03:49 +0200

    upd template show, add style

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,24 +2,49 @@
 
 @section('content')
 
-<a href="/">Back</a>
-<h3>{{ $file->name }} ({{ $file->dates }})</h3>
+<style>
+    .container {
+        border: 8px;
+        padding: 10px;
+    }
 
-<div>Description: {{ $file->description }}</div>
+    .preview-container {
+        width: 50%;
+    }
 
-<div>Date: {{ $file->dates }}</div>
-<div>Price: ${{ $file->price }}</div>
-<div>
-    <h5>Preview</h5>
-    <iframe src="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" width="400"></iframe>
+    .info-container {
+        width: 50%;
+    }
+</style>
+
+<div style="display: flex;">
+    <div>
+        <a href="/">Back</a>
+    </div>
+
+
+    <div class="container preview-container">
+        <div>
+            <h5>Preview</h5>
+            <iframe src="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" style="width: 100%; aspect-ratio: attr(width) / attr(height);"></iframe>
+        </div>
+    </div>
+    <div class="container info-container">
+        <h3>{{ $file->name }}</h3>
+
+        <div>Description: {{ $file->description }}</div>
+
+        <div>Date: {{ $file->dates }}</div>
+        <div>Price: ${{ $file->price }}</div>
+        <div>
+            <h5>Download</h5>
+            {{-- Link to download the file --}}
+            <a href="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" download="{{ $file->name }}">Download {{ $file->name }}.pdf</a>
+        </div>
+    </div>
+
 </div>
 
-
-<div>
-    <h5>Download</h5>
-    {{-- Link to download the file --}}
-    <a href="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" download="{{ $file->name }}">Download {{ $file->name }}.pdf</a>
-</div>
 
 
 @endsection
