396ad60ab902e54f3b6d063d8a123a7640aab26f
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-04 20:36:07 +0200

    add cdn bootstrap, upd templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,8 @@
     @foreach ($files as $file)
     <li><a href="{{ route('show', ['id' => $file->id]) }}">{{ $file->name }}</a></li>
     @endforeach
+    <button type="button" class="btn btn-primary">Primary</button>
+
 </ul>
 
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,46 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
+<div>
+    <a href="/" class="btn btn-secondary">Back</a>
+</div>
+<div class="p-4 d-flex">
 
-<style>
-    .container {
-        border: 8px;
-        padding: 10px;
-    }
 
-    .preview-container {
-        width: 50%;
-    }
 
-    .info-container {
-        width: 50%;
-    }
-</style>
+    <div class="container w-70">
+        <h5>Preview</h5>
+        <iframe src="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" class="w-100 h-100"></iframe>
 
-<div style="display: flex;">
-    <div>
-        <a href="/">Back</a>
     </div>
+    <div class="container w-30">
 
-
-    <div class="container preview-container">
-        <div>
-            <h5>Preview</h5>
-            <iframe src="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" style="width: 100%; aspect-ratio: attr(width) / attr(height);"></iframe>
-        </div>
-    </div>
-    <div class="container info-container">
         <h3>{{ $file->name }}</h3>
 
         <div>Description: {{ $file->description }}</div>
-
         <div>Date: {{ $file->dates }}</div>
         <div>Price: ${{ $file->price }}</div>
         <div>
             <h5>Download</h5>
             {{-- Link to download the file --}}
-            <a href="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" download="{{ $file->name }}">Download {{ $file->name }}.pdf</a>
+            <a href="{{ asset('/storage/uploaded_files/' . basename($file->path)) }}" download="{{ $file->name }}" class="btn btn-primary">Download {{ $file->name }}.pdf</a>
         </div>
+
     </div>
 
 </div>
