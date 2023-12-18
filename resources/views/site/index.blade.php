187150ec8ac2422cacff5166c8e63afe034ceb6e
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-12-18 11:39:50 +0200

    upd style list and show page

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 
-<h1>Catalog</h1>
+<h1>Catalog {{ count($files) }}</h1>
 
 <div class="row">
     @foreach ($files as $file)
-    <div class="col-md-4 mb-4">
-        <div class="card border-0 bg-white">
+    <div class="col-md-4 mb-4" class="">
+        <div class="card bg-white border rounded-2">
             <img src="{{ asset('storage/' . $file->thumbnail) }}" class="card-img-top mx-auto" height='240px' alt="image">
-            <div class="card-body text-center">
+            <div class="card-body">
+                <h3>{{ $file->name }}</h3>
                 <a href="{{ route('site.show', ['slug' => $file->slug]) }}" class="btn btn-outline-primary">
-                    {{ $file->name }}
+                    View
                 </a>
             </div>
         </div>
